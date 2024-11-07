79de0466079c39faa897f09df6745842bfb0b668
Yoann Sousa <yoannsousa@laposte.net>
2024-11-07 16:09:08 +0100

    Initial commit - setup project structure

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from 'react';
+
+
+function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('/api/hello')
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message))
+      .catch((err) => console.error(err));
+  }, []);
+
+  return (
+    <div>
+      <h1>Mon Portfolio</h1>
+      <p>{message}</p>
+    </div>
+  );
+}
+
+export default App
---
+++ server/app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log('Requête reçue !');
+  next();
+});
+
+app.use((req, res, next) => {
+  res.status(201);
+  next();
+});
+
+// Servir les fichiers statiques du front-end React
+app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
+
+// Route pour les requêtes non gérées par l'API (React Router)
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
+});
+
+app.use((req, res, next) => {
+  res.json({ message: 'Votre requête a bien été reçue !' });
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log('Réponse envoyée avec succès !');
+});
+
+
+module.exports = app;

9f7542438596bb449ce2495a33a7292e1842e8cf
Yoann Sousa <yoannsousa@laposte.net>
2024-11-17 13:19:44 +0100

    secondary fix for rate limiter and css fix

--- server/app.js
+++ server/app.js
@@ -11,8 +11,12 @@ const rateLimit = require('express-rate-limit');
 
 const limiter = rateLimit({
   windowMs: 1 * 60 * 1000, // 1 minute
-  max: 60, // limit each IP to 60 requests per windowMs
+  max: 60, // Limit each IP to 60 requests per windowMs
   message: 'Too many requests from this IP, please try again later.',
+  keyGenerator: (req) => {
+    // Use req.ip to safely extract the client IP
+    return req.ip;
+  },
 });
 
 require('dotenv').config();

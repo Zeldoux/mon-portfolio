ad776660baf7c2a3702c1fc63475c451ee2c7970
Yoann Sousa <yoannsousa@laposte.net>
2024-11-18 01:21:05 +0100

    rollback dennis advices

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import AppRouter from './Router/AppRouter';
 import { AuthProvider } from './Context/AuthContext';
-//import './styles/main.css';
+import './styles/main.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
